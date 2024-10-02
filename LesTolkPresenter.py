967e1f527360616ce379e7a33d17a34c98cdaaee
erljoess <erling.jossang@avinor.no>
2024-10-02 18:16:42 +0200

    Eksperimenterer med stringhåndtering
    leiter etter Sola, am og pm i første omgang

---
+++ LesTolkPresenter.py
@@ -0,0 +1,10 @@
+    #Obligatorisk prosjekt, DAT120 programmering.
+    #Medlemmer: Abayomi Bianca Buys, Erling Jøssang, Noashin Laila, Varvara Kuzmina
+    #Skal hente data frå to csv-filer og presentere dei i eit grafisk brukargrensesnitt.
+    #Filene inneheld tids- og meteorologiske data i ulike format, slik at try-except er nødvendig.
+    #For å komme så langt er det nødvendig å splitte linjer i filene, og deretter splitte elementene i linjene.
+    #Planen er å bruke "datetime" for å konvertere tidsdata til eit format som kan brukes i programmet.
+
+import datetime as dt
+
+
--- Split data from file and make a list.py
+++ Split data from file and make a list.py
@@ -6,31 +6,40 @@ Created on Tue Oct  1 22:18:20 2024
 """
 Time = []
 
-def convert data(filename):
-    with open(filename, "r") as file:
-       next(file)
+def convert_data():
+    with open("test.csv.txt", "r") as file:
+        next(file)
         data = file.read()
 
         for line in data.split("\n"):
             part = line.split(";")
-            date_time = part[0].split(" ")
-            date = date_time[0].split(".")
-            time = date_time[1].split(":")
-            seconds = int(part[1]) % 60
-            Time.append(f"{date[2]};{date[1]};{date[0]};{time[0]};{time[1]};{seconds};{part[2]};{part[3]};{part[4]}")
+            if "am" in part:
+                print("am in part")
+            elif "pm" in part:
+                print("pm in part")
+            elif "Sola" in part:
+                print("Sola in part")
+            else:
+                date_time = part[0].split(" ")
+                date = date_time[0].split(".")
+                time = date_time[1].split(":")
+                seconds = int(part[1]) % 60
+                Time.append(f"{date[2]};{date[1]};{date[0]};{time[0]};{time[1]};{seconds};{part[2]};{part[3]};{part[4]}")
     return Time
 
-data = "06.12.2021 18:11;100090;;101,055;14,85"
+convert_data()
 
-Time = []
+#data = "06.12.2021 18:11;100090;;101,055;14,85"
 
-part = data.split(";")
+#Time = []
 
-date_time = part[0].split(" ")
-date = date_time[0].split(".")
-time = date_time[1].split(":")
-seconds = int(part[1]) % 60
+#part = data.split(";")
 
-Time.append(f"{date[2]};{date[1]};{date[0]};{time[0]};{time[1]};{seconds};{part[2]};{part[3]};{part[4]}")
+#date_time = part[0].split(" ")
+#date = date_time[0].split(".")
+#time = date_time[1].split(":")
+#seconds = int(part[1]) % 60
 
-print(Time)
+#Time.append(f"{date[2]};{date[1]};{date[0]};{time[0]};{time[1]};{seconds};{part[2]};{part[3]};{part[4]}")
+
+#print(Time)
